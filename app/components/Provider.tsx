e0cdc110494d18eda6ac4491fe10e8dbf9a0d4f6
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-06-27 12:16:57 +0530

    created imagekit provider file

---
+++ app/components/Provider.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { SessionProvider } from "next-auth/react";
+// using this we can call session inside any frontend components.
+import { ImageKitProvider } from "@imagekit/next";
+
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <SessionProvider refetchInterval={5 * 60}>
+            <ImageKitProvider urlEndpoint={urlEndPoint}>
+                {children}
+            </ImageKitProvider>
+        </SessionProvider>
+    )
+}
